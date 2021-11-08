df2f2af903aea7579dca0e670cc878adbc4586b9
Hans Becker <2929278+StareInTheAir@users.noreply.github.com>
2021-11-08 11:04:00 +0000

    Disable source maps for production builds

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  productionSourceMap: false
 })
